Inversion has a bug. I get wrong values.

enseisro/functional_fitting/make_inversion_matrices.py
@@ -3,16 +3,54 @@ import numpy as np
 import enseisro.forward_functions_Omega as forfunc_Om
 from enseisro import get_kernels as get_kerns
 
-def make_A(GVAR, modes, Nparams=2, smax=1):
+NAX = np.newaxis
+
+def make_A(GVAR, modes, sigma_arr, rcz=0.7, Nparams=2, smax=1):
     """This function is used to create the A matrix in
     A . a = d for our linear inverse problem
     """
     # B = K_{ij} F_{jk} as defined in the Overleaf notes
-    B_arr = np.zeros((len(modes), Nparams))
+    Nmodes = modes.shape[1]
+    B_arr = np.zeros((Nmodes, Nparams))
 
-    for i in modes
+    # creating s_arr
+    s_arr = np.arange(1, smax+1, 2)
+    
+    # indices of where to start and end in mode-labels when filling B_{ik}
+    modefill_start_ind, modefill_end_ind = 0, 0
 
-def make_d(GVAR, modes, use_synth=True, Omega_synth=np.array([])):
+    i = 0
+    while(i < Nmodes):
+        # extracting the m's from the instantaneous multiplet                                                                                                                           
+        n_inst, ell_inst = modes[0,i], modes[1,i]
+        
+        # array that contains all the m's for this instantaneous multiplet                                                                                                              
+        inst_mult_marr = get_inst_mult_marr(n_inst, ell_inst, modes)
+        
+        # creating the mode end index                                                                                                                                                   
+        modefill_end_ind = modefill_start_ind + len(inst_mult_marr)
+        
+        # getting the K_{ij}F_{jk}
+        KF = get_kerns.compute_kernel(GVAR, np.array([[n_inst, ell_inst]]), rcz, s_arr)    # shape (m x s x Nparams)
+
+        # for now only one s. So, I am doing this explicitly
+        KF = KF[:,0,:]      # shape (m x Nparams)
+
+        B_arr[modefill_start_ind:modefill_end_ind,:] = KF[inst_mult_marr + ell_inst,:]
+
+        # moving onto the next multiplet
+        i += len(inst_mult_marr)
+
+        # moving the mode start index to the next multiplet
+        modefill_start_ind = modefill_end_ind
+
+    # finally we divide by the respective freq splitting uncertainties
+    A = B_arr / sigma_arr[:,NAX]
+
+    return A
+        
+
+def make_d_synth(GVAR, modes, sigma_arr, Omega_synth=np.array([])):
     """This function is used to build the data vector d  of frequency splittings
     in the forward problem A . a = d
     """
@@ -26,13 +64,11 @@ def make_d(GVAR, modes, use_synth=True, Omega_synth=np.array([])):
         # extracting the m's from the instantaneous multiplet
         n_inst, ell_inst = modes[0,i], modes[1,i]
         
-        # extracting all the available m's for this multiplet
-        mask_inst_mult = (modes[0,:]==n_inst)*(modes[1,:]==ell_inst)
         # array that contains all the m's for this instantaneous multiplet
-        inst_mult_marr = modes[:,mask_inst_mult][2,:]
+        inst_mult_marr = get_inst_mult_marr(n_inst, ell_inst, modes)
         
         # computing the forward problem to get the mx1 array of frequency splittings
-        domega_m = forfunc_Om.compute_splitting(GVAR, Omega_synth,np.array([[n_inst,ell_inst]]))
+        domega_m = forfunc_Om.compute_splitting(GVAR, Omega_synth[0], np.array([[n_inst,ell_inst]]))
 
         # appending the necessary entries of m in d
         m_indices = inst_mult_marr + ell_inst
@@ -41,5 +77,22 @@ def make_d(GVAR, modes, use_synth=True, Omega_synth=np.array([])):
         # moving onto the next multiplet
         i += len(inst_mult_marr)
     
+    # scaling by the uncertainty in freq splitting
+    d = d / sigma_arr
     
     return d
+
+
+def get_inst_mult_marr(n_inst, ell_inst, modes):
+    """This function takes the instantaneous n and ell for a mode in the 
+    list of all observed modes and returns the m's corresponding to that 
+    (n_inst, ell_inst) multiplet. This is to avoid repeated kernel computation for 
+    each m which is clearly unnecessary."""
+
+    # extracting all the available m's for this multiplet                                                                                                                            
+    mask_inst_mult = (modes[0,:]==n_inst)*(modes[1,:]==ell_inst)
+    
+    # array that contains all the m's for this instantaneous multiplet                                                                                                               
+    inst_mult_marr = modes[:,mask_inst_mult][2,:]
+
+    return inst_mult_marr

enseisro/functional_fitting/test_make_matrices.py
@@ -7,6 +7,7 @@ from enseisro.functional_fitting import make_inversion_matrices as make_inv_mat
 from enseisro.synthetics import create_synthetic_DR as create_synth_DR
 from enseisro.synthetics import w_omega_functions as w_om_func
 from enseisro import get_kernels as get_kerns 
+from enseisro.functional_fitting import a_solver as a_solver
 
 ARGS = FN.create_argparser()
 GVAR = globalvars.globalVars(ARGS)
@@ -19,17 +20,42 @@ mults = np.array([[2,10], [2,12], [3,14]], dtype='int')
 # getting the modes for which we want splitting values
 modes = make_modes.make_modes(mults)
 
+# creating the uncertainty of splitting vector                                                                                                                                          
+sigma_arr = np.ones(modes.shape[1])
+
+smax = 1
+s_arr = np.arange(1,smax+1,2)
 # extracting the solar DR profile in terms of wsr                                                                                                                                       
-wsr = create_synth_DR.get_solar_DR(GVAR, smax=3)
+wsr = create_synth_DR.get_solar_DR(GVAR, smax=smax)
 # converting this wsr to Omegasr                                                                                                                                                     
 Omegasr = w_om_func.w_2_omega(GVAR, wsr)
 
+# making it  in the (Nstars x s x r) shape
+Omegasr = np.reshape(Omegasr, (1, len(s_arr), len(GVAR.r))) 
 
 # getting the data matrix
-d = make_inv_mat.make_d(GVAR, modes, use_synth=True, Omega_synth=Omegasr)
-
+d = make_inv_mat.make_d_synth(GVAR, modes, sigma_arr, Omega_synth = Omegasr[0])  # using a single star for now                                                                                             
+# print(d * GVAR.OM * 1e9)
 ########## testing the construction of the A matrix ##################
 
 # obtaining the kernel for a certain multiplet
-K = get_kerns.compute_kernel(GVAR, np.array([[2,10]]), s_arr=np.array([1]))
-print(K.shape)
+K = get_kerns.compute_kernel(GVAR, np.array([[2,10]]), 0.7, s_arr)
+# print(K)
+# obtaining the A matrix
+
+A = make_inv_mat.make_A(GVAR, modes, sigma_arr)
+# print(A)
+
+# finding the rcz index                           
+rcz = 0.7
+rcz_ind = np.argmin(np.abs(GVAR.r - rcz))
+rcz_ind_arr = np.array([rcz_ind])   # contains only one star 
+
+# calculating the step function equivalent of this Omegasr                                                                                                                              
+step_param_arr = create_synth_DR.get_solar_stepfn_params(Omegasr, rcz_ind_arr)   # shape (Nstars x Nparams x s)                                                                         
+Omegasr_step = create_synth_DR.params_to_step(GVAR, step_param_arr, rcz_ind_arr)
+
+# solving for a in A . a = d
+a = a_solver.use_numpy_inv(GVAR, modes, sigma_arr, smax, use_synth=True, Omegasr = Omegasr_step)
+# print(Omegasr_step * GVAR.OM * 1e9)
+print(a * GVAR.OM * 1e9)

enseisro/get_kernels.py
@@ -6,7 +6,7 @@ import sys
 NAX = np.newaxis
 
 # {{{ def compute_kernel():                                                                                                                                                           
-def compute_kernel(GVAR, mult, Nparams = 2, rcz = 0.7, s_arr = np.array([1], dtype='int')):
+def compute_kernel(GVAR, mult, rcz, s_arr, Nparams = 2):
         """Function to compute the frequency splittings                                                                                                                                  
         under isolated multiplet approximation.                                                                                                                                          
         - GVAR: Dictionary of global parameters                                                                                                                                          
@@ -27,11 +27,12 @@ def compute_kernel(GVAR, mult, Nparams = 2, rcz = 0.7, s_arr = np.array([1], dty
 
         prod_gammas = FN.gamma(ell) * FN.gamma(ell)  # * FN.gamma(s_arr), not using for Omega
 
-        Tsr = compute_Tsr(GVAR, mult, mult, s_arr)
+        Tsr = compute_Tsr(GVAR, mult, mult, s_arr)   # shape (s x r)
 
         # summing over the radial grid to do K_{ij} F_{jk}. See notes in Overleaf
         # the idea is that since F_{jk} are step functions going from 0 to 1,
         # it implies a summation of the kernels before and after the rcz
+
         K = np.zeros((len(s_arr), Nparams))    # shape (s x Nparams)
 
         # writing this for just two params for the step function case

enseisro/synthetics/create_synthetic_DR.py
@@ -5,7 +5,7 @@ NAX = np.newaxis
 
 WFNAME = 'w_s/w.dat'
 
-# {{{ get_solar_DR():
+# {{{ def get_solar_DR():
 def get_solar_DR(GVAR, smax=3):
     '''Function to load and return the Solar differential rotation
     in the w_s(r) form for s=1,3 by default
@@ -17,8 +17,9 @@ def get_solar_DR(GVAR, smax=3):
     wsr = wsr[:(smax-1)//2 + 1]
 
     return wsr
-# }}} get_solar_DR()
+# }}} def get_solar_DR()
 
+# {{{ def make_DR_copies_random():
 def make_DR_copies_random(GVAR, wsr, N=10, p=1):
     '''This function returns multiple copies of
     an input DR profile input in the w_s(r) format'''
@@ -38,6 +39,39 @@ def make_DR_copies_random(GVAR, wsr, N=10, p=1):
     wsr_ens *= (1 + rand_arr * p * 0.01)[NAX, NAX, :]
 
     return wsr_ens
-    
+# }}} def make_DR_copies_random()
 
+# {{{ def make_solar_stepfn():    
+def get_solar_stepfn_params(Omegasr, rcz_ind_arr, Nparams=2):
+    """This function receives the Omega_s(r) for a number of stars
+    and returns the corresponding step functions by averaging the 
+    interior and exterior zones (about the rcz).
+    """
+    # Omegasr needs to be passed in the shape (Nstars x Nparams x s)
+
+    # finding the number of stars passed
+    Nstars, lens = Omegasr.shape[0], Omegasr.shape[1]
     
+    # creating the array to store \Omega_{out} and \Omega_{out} + \Delta \Omega
+    step_param_arr = np.zeros((Nstars, Nparams, lens))   # shape (Nstars x Nparams x s)
+
+    for star_ind in range(Nstars):
+        step_param_arr[star_ind, 0, :] = np.mean(Omegasr[star_ind,:,:rcz_ind_arr[star_ind]], axis=1)
+        step_param_arr[star_ind, 1, :] = np.mean(Omegasr[star_ind,:,rcz_ind_arr[star_ind]:], axis=1)
+                              
+    return step_param_arr
+# }}} def_make_solar_stepfn_params()
+
+def params_to_step(GVAR, step_param_arr, rcz_ind_arr):
+    Nstars, lens = step_param_arr.shape[0], step_param_arr.shape[2]
+    Omegasr_step = np.zeros((Nstars,lens,len(GVAR.r)))
+
+    for s_ind in range(lens):
+        for star_ind in range(Nstars):
+            rcz_ind = rcz_ind_arr[star_ind]
+            print(star_ind, s_ind, rcz_ind)
+            Omegasr_step[star_ind,s_ind,:rcz_ind] = step_param_arr[star_ind,0,s_ind]
+            Omegasr_step[star_ind,s_ind,rcz_ind:] = step_param_arr[star_ind,1,s_ind]
+
+    # returning shape (Nstars x s x r)
+    return Omegasr_step