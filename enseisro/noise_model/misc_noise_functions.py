4df52148fd3b9cc3b3c3d40dba3bbb048f5cc472
srijaniiserprinceton <sbdas@princeton.edu>
2021-07-08 16:47:36 -0400

    Generated gh-pages for commit b86c19f Author: srijaniiserprinceton <sbdas@princeton.edu>
        Minor change in docstring

--- enseisro/noise_model/misc_noise_functions.py
+++ enseisro/noise_model/misc_noise_functions.py
@@ -7,11 +7,16 @@ import numpy as np
 # {{{ def make_N_nu():
 def make_N_nu(nu_arr, tau_arr, A_arr):
     """This function returns $N(\nu)$ from a given frequency array $\nu$..
+
     Parameters
     ----------
-    nu_arr : float array
-    tau_arr : float array
-    A_arr : float array
+    nu_arr : numpy.ndarray
+             frequency array          
+    tau_arr : numpy.ndarray
+              tau array
+    A_arr : numpy.ndarray
+            amplitude array
+
     """
 
 # }}} def make_N_nu()
