eef15a83be8f60f9f7da0ed983b693aa947788af
srijaniiserprinceton <sbdas@princeton.edu>
2021-07-08 20:49:02 -0400

    Added the envelope function and the mode visilibity

--- enseisro/noise_model/misc_noise_functions.py
+++ enseisro/noise_model/misc_noise_functions.py
@@ -1,5 +1,7 @@
 # import jax.numpy as np
 import numpy as np
+from scipy.special import lpmv
+from math import factorial
 
 # this file contains miscellaneous functions to construct the Noise model.
 # the nomenclature is adopted from Thorsten Stahn's PhD thesis.
@@ -18,7 +20,7 @@ def make_N_nu(nu_arr, tau_arr, A_arr, P_wn, return_harveys=False):
         Amplitude array in ppm^2 muHz^{-1}
     P_wn : scalar
         Photon white noise in ppm^2 muHz^{-1}
-    return_harvets : boolean
+    return_harveys : boolean, optional
         True or False depending on whether or not we want to 
         return the Harvey models separately. This is mainly used for 
         plotting them separately when tallying with Stahn's Figure 2.4.
@@ -45,3 +47,61 @@ def make_N_nu(nu_arr, tau_arr, A_arr, P_wn, return_harveys=False):
     else: return N_nu_arr
 
 # }}} def make_N_nu()
+
+
+# {{{ def mode_visibility():
+def mode_visibility(abs_m_arr, ell_arr, inc_angle_arr):
+    """This computes the visibility of individual m-components
+    depending on the inclination angle i, of the rotation axis
+    with respect to the line of sight.
+
+    Parameters
+    ----------
+    abs_m_arr : int, array_like
+        Array containing |m|.
+    ell_arr : int, array_like
+        Array containing \ell.
+    inc_angle_arr : float, array_like
+        Array contiaining the inclination angle of the rotation axis 
+        of the star with respect to the line of sight. Array has the 
+        same length as the length of the abs_m_arr or ell_arr.
+    """
+
+    # vectorizing the math.factorial function
+    fac_vec = np.vectorize(factorial)
+    prefac = fac_vec(ell_arr - abs_m_arr)/fac_vec(ell_arr + abs_m_arr) 
+
+    eps_ell_m = prefac * lpmv(abs_m_arr, ell_arr, inc_angle_arr)**2
+
+    # should have the same length as the ell_arr or abs_m_arr
+    return eps_ell_m
+# }}} def mode_visibility()
+
+# {{{ def envelope_function():
+def envelope_function(mode_freq, cen_freq, sigma_1, sigma_2):
+    """Calculating the envelope function as given by Eqn.~(2.6) 
+    in Stahn's thesis.
+    
+    Parameters
+    ----------
+    mode_freq : float, array_like
+        The mode frequencies \nu_{n,\ell,m}
+    cen_freq : float
+        The frequency of the center of the amplitude envelope.
+    sigma_1 : float
+        Width of the envelope below cen_freq.
+    sigma_2 : float
+        Width of the envelope above cen_freq.
+    """
+    # creating a sigma array depending on relative value of
+    # mode_freq and cen_freq
+    sigma_arr = np.zeros_like(mode_freq)
+
+    ind_where_modefreq_leq_cenfreq = mode_freq < cen_freq
+    sigma_arr[ind_where_modefreq_leq_cenfreq] = sigma_1
+    sigma_arr[~ind_where_modefreq_leq_cenfreq] = sigma_2
+    
+    # has the same shape as the mode_freq
+    F = (1 + ((mode_freq - cen_freq)/sigma_arr)**2)**(-2)
+
+    return F
