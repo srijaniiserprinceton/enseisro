cbd153dd04c04ce0a36109cbbce7eabe24d23a6d
srijaniiserprinceton <sbdas@princeton.edu>
2022-04-22 21:06:25 -0400

    minor changes

--- jax_enseisro/build_star_info.py
+++ jax_enseisro/build_star_info.py
@@ -22,15 +22,15 @@ rand_mults : Whether to introduce minor changes across the radial order of
 #-------------------------- defining metadata -----------------------------#
 nStype = 4
 
-num_startype_arr = np.array([1e2, 1.4e2, 1.1e2, 1.2e2], dtype='int')
+num_startype_arr = np.array([1, 1, 1, 1], dtype='int')
 
 # rcz_startype_arr = np.array([0.68, 0.71, 0.7, 0.69])
 rcz_startype_arr = np.array([0.7, 0.7, 0.7, 0.7])
 
-nmin, nmax = 16, 24
-lmin, lmax = 1, 3
+nmin, nmax = 20, 20
+lmin, lmax = 1, 1
 
-rand_mults = 1
+rand_mults = 0
 
 # Max angular degree for Omega_s
 smax = 3
